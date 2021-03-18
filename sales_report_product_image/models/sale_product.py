09a1d6eb9788aa7c3450390ecfbd76248f326f9a
Joel Gutierrez <40271253+joelega@users.noreply.github.com>
2021-03-18 12:38:49 -0500

    update imagen

--- sales_report_product_image/models/sale_product.py
+++ sales_report_product_image/models/sale_product.py
@@ -7,20 +7,13 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     print_image = fields.Boolean(
-        "Print Image",
-        help="""If ticked, you can see the product image in
-        report of sale order/quotation""",
-    )
+        'Con Imagenes', help="""If ticked, you can see the product image in
+        report of sale order/quotation""")
     image_sizes = fields.Selection(
-        [
-            ("image", "Big sized Image"),
-            ("image_medium", "Medium Sized Image"),
-            ("image_small", "Small Sized Image"),
-        ],
-        "Image Sizes",
-        default="image_small",
-        help="Image size to be displayed in report",
-    )
+        [('image', 'Imagen Grande'), ('image_medium', 'Imagen Mediana'),
+         ('image_small', 'Imagen Chica')],
+        'Tamaño de imagen', default="image_small",
+        help="Image size to be displayed in report")
 
 
 class SaleOrderLine(models.Model):
